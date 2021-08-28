27c3800f4652d18831b866c5113e1628f8e291b8
bestpain <nitishranjan67@gmail.com>
2021-08-28 17:31:32 +0530

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import { Link } from 'gatsby'
+import React from 'react'
+import Layout from '../components/layout'
+
+export default function About() {
+    return (
+        <Layout pageTitle="About me">
+            About me is not good
+        </Layout>
+    )
+}
